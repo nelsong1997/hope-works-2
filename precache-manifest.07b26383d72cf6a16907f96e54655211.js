082d3b98d67b79b889c95860e396f1ac7a29770e
Gabe N <nelsong1997@gmail.com>
2020-05-06 18:48:12 -0400

    Updates

---
+++ precache-manifest.07b26383d72cf6a16907f96e54655211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4228d48340efe649aba69c1e76e32f6e",
+    "url": "/hope-works-2/index.html"
+  },
+  {
+    "revision": "72796100c10403b77b79",
+    "url": "/hope-works-2/static/js/2.d3cd4255.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hope-works-2/static/js/2.d3cd4255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ffc3e4c4d3927f8c092",
+    "url": "/hope-works-2/static/js/main.5ac511f7.chunk.js"
+  },
+  {
+    "revision": "af307e51b55ec70dd3b2",
+    "url": "/hope-works-2/static/js/runtime-main.26f2e641.js"
+  },
+  {
+    "revision": "c671b095543037c9db270758df4aaa78",
+    "url": "/hope-works-2/static/media/hw.c671b095.png"
+  }
+]);
